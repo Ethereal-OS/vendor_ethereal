f4fdd85257248082202abbcddefbe620233a734f
IamCOD3X <sourabh.panchal28@gmail.com>
2024-08-04 23:38:08 +0530

    Changelog script

--- build/tools/changelog.sh
+++ build/tools/changelog.sh
@@ -21,31 +21,31 @@ DEVICE=$1
 
 if [ -f $Changelog ];
 then
-	rm -f $Changelog
+    rm -f $Changelog
 fi
 
 touch $Changelog
 
 # define changelog_days using 'export changelog_days=10'
-# this can be done before intiate build environment (. build/envsetup.sh)
+# this can be done before initiate build environment (. build/envsetup.sh)
 if [ -z $changelog_days ];then
-	changelog_days=10
+    changelog_days=10
 else
-	if (($changelog_days > 30 )); then
-        echo "Changelog can not generated for more than 30 days. For how many days do you want to generate changelog again? (🕑 timeout 15 seconds - default to 10 days)"
+    if (($changelog_days > 30 )); then
+        echo "Changelog cannot be generated for more than 30 days. For how many days do you want to generate changelog again? (🕑 timeout 15 seconds - default to 10 days)"
         read -r -t 15 changelog_days || changelog_days=10
-	fi
+    fi
 fi
 
 REPO_LIST="$(cat .repo/project.list | sed '\?^vendor/officialdevices?d')"
 for i in $(seq $changelog_days); do
-    After_Date=`date --date="$i days ago" +%m-%d-%Y`
+    After_Date=$(date -d "$i days ago" +"%m-%d-%Y")
     k=$(expr $i - 1)
-    Until_Date=`date --date="$k days ago" +%m-%d-%Y`
+    Until_Date=$(date -d "$k days ago" +"%m-%d-%Y")
 
     # Line with after --- until was too long for a small ListView
     echo '====================' >> $Changelog
-    echo  "     "$Until_Date    >> $Changelog
+    echo "     "$Until_Date >> $Changelog
     echo '====================' >> $Changelog
 
     # Cycle through all available repos
@@ -62,5 +62,13 @@ done
 
 sed -i 's/project/   */g' $Changelog
 
-cp $Changelog $OUT_DIR/target/product/$DEVICE/system/etc/
-mv $Changelog $OUT_DIR/target/product/$DEVICE/
+if [ -f "$Changelog" ]; then
+    if cp "$Changelog" "$OUT_DIR/target/product/$DEVICE/system/etc/"; then
+        mv "$Changelog" "$OUT_DIR/target/product/$DEVICE/${DEVICE}_changelog.txt" || echo "Failed to move changelog file to $OUT_DIR/target/product/$DEVICE/${DEVICE}_changelog.txt"
+    else
+        echo "Failed to copy changelog file to $OUT_DIR/target/product/$DEVICE/system/etc/"
+    fi
+else
+    echo "Changelog file does not exist"
+fi
+
