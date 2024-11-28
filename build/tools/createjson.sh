47dff295eddeb817bb7b32812c3babac54cd7f1a
IamCOD3X <sourabh.panchal28@gmail.com>
2024-11-28 14:19:30 +0530

    device json logic update

--- build/tools/createjson.sh
+++ build/tools/createjson.sh
@@ -1,8 +1,10 @@
+#!/bin/bash
 #
-# Copyright (C) 2019 crDroid Android Project
+# Copyright (C) 2024 Ethereal Android Project
 #
-# Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License. You may obtain a copy of the 
-# License at
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
 #
 # http://www.apache.org/licenses/LICENSE-2.0
 #
@@ -14,8 +16,13 @@
 #
 
 #$1=TARGET_DEVICE, $2=PRODUCT_OUT, $3=LINEAGE_VERSION
-existingOTAjson=./vendor/ota/$1.json
-output=./vendor/ota/$1.json
+existingOTAjson=./vendor/officialdevices/devices/$1.json
+output=$2/$1.json
+
+#cleanup old file
+if [ -f $output ]; then
+	rm $output
+fi
 
 if [ -f $existingOTAjson ]; then
 	#get data from already existing device json
@@ -24,39 +31,93 @@ if [ -f $existingOTAjson ]; then
 	oem=`grep -n "\"oem\"" $existingOTAjson | cut -d ":" -f 3 | sed 's/"//g' | sed 's/,//g' | xargs`
 	device=`grep -n "\"device\"" $existingOTAjson | cut -d ":" -f 3 | sed 's/"//g' | sed 's/,//g' | xargs`
 	filename=$3
-	version=`echo "$3" | cut -d'-' -f2`
-	download="https://sourceforge.net/projects/ethereal-os/files/$1/$filename/download"
+	version=`echo "$3" | cut -d'-' -f5`
+	v_max=`echo "$version" | cut -d'.' -f1 | cut -d'v' -f2`
+	v_min=`echo "$version" | cut -d'.' -f2`
+	version="$3"
+	download="https://sourceforge.net/projects/ethereal-os/files/$1/$3/download"
 	buildprop=$2/system/build.prop
 	linenr=`grep -n "ro.system.build.date.utc" $buildprop | cut -d':' -f1`
 	timestamp=`sed -n $linenr'p' < $buildprop | cut -d'=' -f2`
 	md5=`md5sum "$2/$3" | cut -d' ' -f1`
+	sha256=`sha256sum "$2/$3" | cut -d' ' -f1`
 	size=`stat -c "%s" "$2/$3"`
-
-	#cleanup old file
-	if [ -f $output ]; then
-        	rm $output
+	buildtype=`grep -n "\"buildtype\"" $existingOTAjson | cut -d ":" -f 3 | sed 's/"//g' | sed 's/,//g' | xargs`
+	forum=`grep -n "\"forum\"" $existingOTAjson | cut -d ":" -f 4 | sed 's/"//g' | sed 's/,//g' | xargs`
+	if [ ! -z "$forum" ]; then
+		forum="https:"$forum
+	fi
+	firmware=`grep -n "\"firmware\"" $existingOTAjson | cut -d ":" -f 4 | sed 's/"//g' | sed 's/,//g' | xargs`
+	if [ ! -z "$firmware" ]; then
+		firmware="https:"$firmware
+	fi
+	modem=`grep -n "\"modem\"" $existingOTAjson | cut -d ":" -f 4 | sed 's/"//g' | sed 's/,//g' | xargs`
+	if [ ! -z "$modem" ]; then
+		modem="https:"$modem
+	fi
+	bootloader=`grep -n "\"bootloader\"" $existingOTAjson | cut -d ":" -f 4 | sed 's/"//g' | sed 's/,//g' | xargs`
+	if [ ! -z "$bootloader" ]; then
+		bootloader="https:"$bootloader
+	fi
+	recovery=`grep -n "\"recovery\"" $existingOTAjson | cut -d ":" -f 4 | sed 's/"//g' | sed 's/,//g' | xargs`
+	if [ ! -z "$recovery" ]; then
+		recovery="https:"$recovery
+	fi
+	buymeacoffee=`grep -n "\"buymeacoffee\"" $existingOTAjson | cut -d ":" -f 4 | sed 's/"//g' | sed 's/,//g' | xargs`
+	if [ ! -z "$buymeacoffee" ]; then
+		buymeacoffee="https:"$buymeacoffee
+	fi
+	telegram=`grep -n "\"telegram\"" $existingOTAjson | cut -d ":" -f 4 | sed 's/"//g' | sed 's/,//g' | xargs`
+	if [ ! -z "$telegram" ]; then
+		telegram="https:"$telegram
+	fi
+	dt=`grep -n "\"dt\"" $existingOTAjson | cut -d ":" -f 4 | sed 's/"//g' | sed 's/,//g' | xargs`
+	if [ ! -z "$dt" ]; then
+		dt="https:"$dt
+	fi
+	common=`grep -n "\"common-dt\"" $existingOTAjson | cut -d ":" -f 4 | sed 's/"//g' | sed 's/,//g' | xargs`
+	if [ ! -z "$common" ]; then
+		common="https:"$common
+	fi
+	kernel=`grep -n "\"kernel\"" $existingOTAjson | cut -d ":" -f 4 | sed 's/"//g' | sed 's/,//g' | xargs`
+	if [ ! -z "$kernel" ]; then
+		kernel="https:"$kernel
 	fi
 
 	echo '{
-  "response": [
-    {
-        "maintainer": "'$maintainer'",
-        "oem": "'$oem'",
-        "device": "'$device'",
-        "filename": "'$filename'",
-        "download": "'$download'",
-        "timestamp": '$timestamp',
-        "md5": "'$md5'",
-        "size": '$size',
-        "version": "'$version'"
-    }
-  ]
+	"response": [
+		{
+			"maintainer": "'$maintainer'",
+			"oem": "'$oem'",
+			"device": "'$device'",
+			"filename": "'$filename'",
+			"download": "'$download'",
+			"timestamp": '$timestamp',
+			"md5": "'$md5'",
+			"sha256": "'$sha256'",
+			"size": '$size',
+			"version": '$version',
+			"buildtype": "'$buildtype'",
+			"forum": "'$forum'",
+			"firmware": "'$firmware'",
+			"modem": "'$modem'",
+			"bootloader": "'$bootloader'",
+			"recovery": "'$recovery'",
+			"buymeacoffee": "'$buymeacoffee'",
+			"telegram": "'$telegram'",
+			"dt": "'$dt'",
+			"common-dt": "'$common'",
+			"kernel": "'$kernel'"
+		}
+	]
 }' >> $output
 
-        echo "vendor/ota/$1.json"
+        echo "JSON file data for OTA support:"
+        echo "vendor/officialdevices/devices/$1.json"
 else
 	#if not already supported, create dummy file with info in it on how to
 	echo 'There is no official support for this device yet' >> $output;
 fi
 
+cat $output
 echo ""
