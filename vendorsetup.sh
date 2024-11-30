a142097e355c745fcc128c438248d6c80e8fc628
IamCOD3X <sourabh.panchal28@gmail.com>
2024-12-01 00:57:04 +0530

    fix: removing extra

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,6 +1,3 @@
-# Read the aosp_target_release from the file, ignoring comments
-aosp_target_release_value=$(grep -oP '^aosp_target_release=\K.*' ./vars/aosp_target_release)
-
 # Initialize the lunch targets array
 lunch_others_targets=()
 
@@ -12,10 +9,6 @@ do
         lunch_others_targets+=("ethereal_${device}-${aosp_target_release_value}-${var}")
     done
 done
-
-# (Optional) Print the generated targets for verification
-echo "Generated lunch targets:"
-printf "%s\n" "${lunch_others_targets[@]}"
 
 # For now, just skip the ABI checks to fix build errors.
 export SKIP_ABI_CHECKS=true
