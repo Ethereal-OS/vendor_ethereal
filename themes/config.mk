60ddcb864240b84ab54287a2b5fe6943e9c0e795
IamCOD3X <sourabh.panchal28@gmail.com>
2025-03-26 15:44:04 +0530

    add: Brightness Slider Overlays

--- themes/config.mk
+++ themes/config.mk
@@ -36,3 +36,18 @@ PRODUCT_PACKAGES += \
     ShishuImmensityTheme \
     ShishuNightsTheme \
     SolarizedDarkTheme
+    
+# Brightness slider styles
+PRODUCT_PACKAGES += \
+    BrightnessSliderAcunOverlay \
+    BrightnessSliderBangOverlay \
+    BrightnessSliderCyberpunkOverlay \
+    BrightnessSliderFilledOverlay \
+    BrightnessSliderGradientRoundedBarOverlay \
+    BrightnessSliderLeafyOutlineOverlay \
+    BrightnessSliderMinimalThumbOverlay \
+    BrightnessSliderOutlineOverlay \
+    BrightnessSliderRoundedClipOverlay \
+    BrightnessSliderShadedOverlay \
+    BrightnessSliderThinOverlay \
+    BrightnessSliderTranslucentOverlay
